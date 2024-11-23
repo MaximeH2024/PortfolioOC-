c64871a5e1be102cbeb2b5799c654992e88b6c90
MaximeH2024 <mhouguet91@gmail.com>
2024-11-23 14:54:10 +0100

    rectification du collapse ProjectDisplay.

--- src/components/ProjectDisplay/ProjectDisplay.jsx
+++ src/components/ProjectDisplay/ProjectDisplay.jsx
@@ -11,8 +11,7 @@ import openclassroomLogo from "../../assets/openclassroom_logo.webp";
 function ProjectDisplay() {
   const [projects, setProjects] = useState([]);
   const { language } = useLanguage();
-  const [showTechnicalSkills, setShowTechnicalSkills] = useState(false);
-  const [showSoftSkills, setShowSoftSkills] = useState(false);
+  const [expandedCards, setExpandedCards] = useState({});
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -28,79 +27,85 @@ function ProjectDisplay() {
   const technicalSkillsLabel = language === "fr" ? "Compétences techniques" : "Technical Skills";
   const softSkillsLabel = language === "fr" ? "Compétences interpersonnelles" : "Soft Skills";
 
+  // Fonction pour gérer le déploiement des compétences
+  const toggleSection = (index, section) => {
+    setExpandedCards((prevState) => ({
+      ...prevState,
+      [index]: {
+        ...prevState[index],
+        [section]: !prevState[index]?.[section],
+      },
+    }));
+  };
+
   return (
     <div className="project-grid">
-      {projects.map((project, index) => {     
-
-        return (
-          <div key={index} className="project-card">
-            <div className="project-icons">
-              <img
-                src={project.type.fr === "formation" ? openclassroomLogo : null}
-                alt="Type Logo"
-                className="type-logo"
-                style={{ display: project.type.fr === "formation" ? "inline-block" : "none" }}
-              />
-              {project.type.fr !== "formation" && (
-                <FontAwesomeIcon icon={faCode} className="type-icon" />
-              )}
-              <FontAwesomeIcon
-                icon={faCircleCheck}
-                className={`status-icon ${project.status.en === "Validated" ? "validated" : "not-validated"}`}
-              />
-              {project.github_link && (
-                <a
-                  href={project.github_link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="github-link"
-                >
-                  <FontAwesomeIcon icon={faGithub} className="github-icon" />
-                </a>
-              )}
-            </div>
-
-            <h3>{project.name[language]}</h3>
-            <p>{project.description[language]}</p>
-
-            {/* Section des compétences techniques */}
-            <div className="skills">
-              <strong
-                className="collapsible-label"
-                onClick={() => setShowTechnicalSkills(!showTechnicalSkills)}
+      {projects.map((project, index) => (
+        <div key={index} className="project-card">
+          <div className="project-icons">
+            <img
+              src={project.type.fr === "formation" ? openclassroomLogo : null}
+              alt="Type Logo"
+              className="type-logo"
+              style={{ display: project.type.fr === "formation" ? "inline-block" : "none" }}
+            />
+            {project.type.fr !== "formation" && (
+              <FontAwesomeIcon icon={faCode} className="type-icon" />
+            )}
+            <FontAwesomeIcon
+              icon={faCircleCheck}
+              className={`status-icon ${project.status.en === "Validated" ? "validated" : "not-validated"}`}
+            />
+            {project.github_link && (
+              <a
+                href={project.github_link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="github-link"
               >
-                {technicalSkillsLabel} +
-              </strong>
-              <ul
-                className={`collapsible-content ${
-                  showTechnicalSkills ? "expanded" : "collapsed"
-                }`}
-              >
-                {project.technical_skills[language].map((skill, i) => (
-                  <li key={i}>{skill}</li>
-                ))}
-              </ul>
-
-              {/* Section des compétences interpersonnelles */}
-              <strong
-                className="collapsible-label"
-                onClick={() => setShowSoftSkills(!showSoftSkills)}
-              >
-                {softSkillsLabel} +
-              </strong>
-              <ul
-                className={`collapsible-content ${
-                  showSoftSkills ? "expanded" : "collapsed"
-                }`}
-              >
-                {project.soft_skills[language].map((skill, i) => (
-                  <li key={i}>{skill}</li>
-                ))}
-              </ul>
-            </div>
+                <FontAwesomeIcon icon={faGithub} className="github-icon" />
+              </a>
+            )}
           </div>
-        );
-      })}
+
+          <h3>{project.name[language]}</h3>
+          <p>{project.description[language]}</p>
+
+          <div className="skills">
+            <strong
+              className="collapsible-label"
+              onClick={() => toggleSection(index, "technicalSkills")}
+            >
+              {technicalSkillsLabel} +
+            </strong>
+            <ul
+              className={`collapsible-content ${
+                expandedCards[index]?.technicalSkills ? "expanded" : "collapsed"
+              }`}
+            >
+              {project.technical_skills[language].map((skill, i) => (
+                <li key={i}>{skill}</li>
+              ))}
+            </ul>
+
+            <strong
+              className="collapsible-label"
+              onClick={() => toggleSection(index, "softSkills")}
+            >
+              {softSkillsLabel} +
+            </strong>
+            <ul
+              className={`collapsible-content ${
+                expandedCards[index]?.softSkills ? "expanded" : "collapsed"
+              }`}
+            >
+              {project.soft_skills[language].map((skill, i) => (
+                <li key={i}>{skill}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
